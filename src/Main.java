029e20b97ebdbe5a3b81fd90dd38e6d17c608239
Aakash Rajlawat <aakashsky472@gmail.com>
2025-11-12 22:42:25 +0545

    inputFromKeyboard

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Enter any two numbers:");
+        a = s.nextInt();
+        b = s.nextInt();
+        c = a + b;
+        System.out.println("The sum is : " + c);
+
+    }
+}
