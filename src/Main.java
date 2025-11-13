933598951b189fcf4e22df479e675dbfdb0d6709
Aakash Rajlawat <aakashsky472@gmail.com>
2025-11-13 12:32:55 +0545

    Just a name printer

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,10 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int age, yrsToWait;
-        System.out.println("Enter your age :");
-        age = s.nextInt();
-        if (age>18 || age == 18) {
-            System.out.println("You can vote");
-        }
-        else {
-            yrsToWait = 18 - age;
-            System.out.println("No! you still have to wait "+ yrsToWait + " yrs");
-        }
-
+       Scanner s = new Scanner(System.in);
+       String name ;
+        System.out.println("Enter Your Name:");
+        name = s.nextLine();
+        System.out.println("Hello Mr. "+name);
     }
 }
